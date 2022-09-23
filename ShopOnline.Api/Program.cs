11170b57aff093dca086f15ef48593614a60d36e
Veli_dh93 <Veli_dh93@student.itstep.md>
2022-09-23 17:39:28 +0300

    Add project files.

---
+++ ShopOnline.Api/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
+ options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection")));
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
